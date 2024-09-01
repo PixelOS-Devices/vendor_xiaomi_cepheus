d25fb9802f8ecc4a32e4258c7e1c5b3f96e086e4
chematelegram <chfondia@gmail.com>
2024-09-01 21:21:44 +0200

    cepheus: Drop updatable GPU drivers

--- cepheus-vendor.mk
+++ cepheus-vendor.mk
@@ -931,7 +931,6 @@ PRODUCT_PACKAGES += \
     CneApp \
     IWlanService \
     TimeService \
-    com.qualcomm.qti.gpudrivers.msmnile.api30 \
     HotwordEnrollmentOKGoogleHEXAGON \
     HotwordEnrollmentXGoogleHEXAGON \
     PowerOffAlarm \
